757e6e9c400f7df2db305e6afa56b614a504fbe0
nishant kaushik <kaushik.nishant74@gmail.com>
2023-02-16 04:11:21 +0530

    initial code

---
+++ user-management-app/src/Account.js
@@ -0,0 +1,183 @@
+import { useState, useEffect } from 'react';
+import { supabase } from './supabaseClient';
+
+const Account = ({ session }) => {
+  const [loading, setLoading] = useState(true)
+  const [username, setUsername] = useState('')
+  const [allUsers, setAllUsers] = useState([])
+  const [full_name, setFullName] = useState('')
+  const [website, setWebsite] = useState('')
+  
+  const [sessionUserId, setSessionUserId] = useState('')
+  const [id, setId] = useState('')
+  const [editfull_name, setEditFullName] = useState('')
+  const [editwebsite, setEditWebsite] = useState('')
+  const [avatar_url, setAvatarUrl] = useState('')
+  const [editMode, setEditMode] = useState(false)
+  
+  useEffect(() => {
+    setEditMode(false);
+    getProfile()
+    getAllProfile()
+  }, [session])
+
+  const getProfile = async () => {
+    try {
+      setLoading(true)
+      const { user } = session
+
+      let { data, error, status } = await supabase
+        .from('profiles')
+        .select(`username, website, avatar_url,full_name`)
+        .eq('id', user.id)
+        .single()
+
+      if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        
+        setSessionUserId(user.id)
+        setUsername(data.username)
+        setWebsite(data.website)
+        setAvatarUrl(data.avatar_url)
+        setFullName(data.full_name)
+        setId(user.id)
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  const getAllProfile = async () => {
+    try {
+      setLoading(true)
+       
+      let { data, error, status } = await supabase
+        .from('profiles')
+        .select(`id,username, website, avatar_url,full_name`)
+     
+        
+     
+        if (error && status !== 406) {
+        throw error
+      }
+
+      if (data) {
+        setAllUsers(data);
+      }
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+  const setUserToEdit =async (data)=>{
+    setEditMode(true)
+    setId(data.id)
+    setUsername(data.username)
+    setWebsite(data.website)
+    setAvatarUrl(data.avatar_url)
+    setFullName(data.full_name)
+  }
+  const cancelEdit=async ()=>{
+    setEditMode(false)
+    setId('')
+    setEditFullName('')
+   
+    setEditWebsite('')
+  }
+  const updateProfile = async (e) => {
+    e.preventDefault()
+
+    try {
+        
+      setLoading(true)
+      const { user } = session
+ 
+      const updates = {
+        id: id,
+        username,
+        website,
+        full_name,
+        avatar_url,
+        updated_at: new Date(),
+      }
+
+      let { error } = await supabase.from('profiles').upsert(updates)
+
+      if (error) {
+        throw error
+      }
+     
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <div aria-live="polite">
+         <div>Logged In User Email: {session.user.email}</div>
+      {editMode &&
+       
+        <form onSubmit={updateProfile} className="form-widget">
+         
+          <div>
+            <label htmlFor="username">Name</label>
+            <input
+              id="username"
+              type="text"
+              value={username || ''}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+           
+          <div>
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              id="fullName"
+              type="text"
+              value={full_name || ''}
+              onChange={(e) => setFullName(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className="button primary block" disabled={loading}>
+             Edit Profile
+            </button>  
+            <input type="button" onClick={()=>cancelEdit()} className="button primary block" value="Cancel" />
+          </div>
+        </form>
+      }
+      {allUsers.length>0 && <table>
+        <tbody>
+        <tr>
+          <th>Action</th>
+          <th>User Name</th>
+          <th>Full Name</th>
+        </tr>
+        {allUsers.map((val, key) => {
+          return (
+            <tr key={key}>
+             <td> { (sessionUserId == val.id) && <button onClick={() => setUserToEdit(val)}>edit</button>}</td>
+              <td>{val.username}</td>
+              <td>{val.full_name}</td>
+               
+            </tr>
+          )
+        })}
+        </tbody>
+      </table>   
+}
+      <button type="button" className="button block" onClick={() => supabase.auth.signOut()}>
+        Sign Out
+      </button>
+    </div>
+  )
+}
+
+export default Account
